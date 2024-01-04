99a9ec357fdba47002935380d0e6e02dac72d199
hyunseong <rlagustjd2684@gmail.com>
2024-01-04 15:53:35 +0900

    개발환경 맞추기

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO user(userid, password, name, phone_number)
-VALUES('60215678', '1234', '김명지', '01012345678');
-INSERT INTO user(userid, password, name, phone_number)
-VALUES('60205678', '1234', '최명지', '01012345677');
-INSERT INTO user(userid, password, name, phone_number)
-VALUES('60195678', '1234', '박명지', '01022345678');
