850d4b1600ebec46f461a1b062717a9fd06c4aae
hyunseong <rlagustjd2684@gmail.com>
2024-01-04 17:38:58 +0900

    더미데이터 빠진거 다시 채워넣기

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -9,4 +9,9 @@ CREATE TABLE IF NOT EXISTS user (
 
 ALTER TABLE user MODIFY COLUMN id INT AUTO_INCREMENT;
 
-
+INSERT INTO user(userid, password, name, phone_number)
+VALUES('60215678', '1234', '김명지', '01012345678');
+INSERT INTO user(userid, password, name, phone_number)
+VALUES('60205678', '1234', '최명지', '01012345677');
+INSERT INTO user(userid, password, name, phone_number)
+VALUES('60195678', '1234', '박명지', '01022345678');
