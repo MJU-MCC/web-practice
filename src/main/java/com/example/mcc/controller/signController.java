4a8ad8242a2d5f16a78e6079787e216c0e82a88b
hyunseong <rlagustjd2684@gmail.com>
2024-02-08 09:46:53 +0900

    스웨거 라이브러리 기능추가

--- src/main/java/com/example/mcc/controller/signController.java
+++ src/main/java/com/example/mcc/controller/signController.java
@@ -3,6 +3,8 @@ package com.example.mcc.controller;
 import com.example.mcc.Dto.memberDto;
 import com.example.mcc.response.SignResponse;
 import com.example.mcc.service.MccSignService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.example.mcc.response.Message.SIGN_FAIL;
 import static com.example.mcc.response.Message.SIGN_SUCCESS;
 
+@Api(value = "MCC 회원가입 API")
 @RestController
 @RequestMapping("/mcc")
 @Slf4j
@@ -28,6 +31,7 @@ public class signController {
 
 
     // 회원가입
+    @ApiOperation(value = "회원가입 Api" ,notes = "학번 , 휴대전화 뒷자리 , role")
     @PostMapping("/sign")
     public ResponseEntity<SignResponse> sign(@RequestBody memberDto member){
 
